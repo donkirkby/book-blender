b9d580b339d7233ef23878d89ce2a52710091071
Don Kirkby <donkirkby@gmail.com>
2024-03-20 10:24:47 -0700

    Draw puzzle grid.

--- blender_block.py
+++ blender_block.py
@@ -5,6 +5,9 @@ from textwrap import wrap
 
 from markdown import Extension, Markdown
 from markdown.treeprocessors import Treeprocessor
+from svgwrite import Drawing
+from svgwrite.shapes import Rect
+from svgwrite.text import Text
 
 
 class BlenderBlockProcessor(Treeprocessor):
@@ -17,7 +20,7 @@ class BlenderBlockProcessor(Treeprocessor):
     def run(self, root):
         texts = (re.sub(r'\s+', ' ', child.text)
                  for child in root)
-        lines = (line
+        lines = (line + ' '*(self.width - len(line))
                  for text in texts
                  for line in wrap(text, self.width))
         groups = zip_longest(*([lines] * self.height))
@@ -52,3 +55,58 @@ class BlenderBlock:
 
     def __init__(self, lines: tuple[str, ...]) -> None:
         self.lines = lines
+
+    def as_svg(self) -> str:
+        column_count = len(self.lines[0])
+        row_count = len(self.lines)
+        letter_width = 14
+        line_height = 20
+        margin = letter_width / 1.4
+        width = column_count * letter_width + 2 * margin
+        height = line_height * (row_count * 5)
+        drawing = Drawing(size=(width, height))
+        for i in range(5, column_count, 10):
+            drawing.add(Rect((i * letter_width + margin, 0),
+                             (5 * letter_width, height),
+                             fill='rgb(200, 200, 200)'))
+        drawing.add(Rect((0, 0),
+                         (width, height),
+                         fill_opacity=0,
+                         stroke='black'))
+        drawing.add(Rect((0, 0),
+                         (width, line_height * (row_count * 4 - 1)),
+                         fill_opacity=0,
+                         stroke='black'))
+        column_letters = [[] for _ in range(column_count)]
+        for i, line in enumerate(self.lines):
+            lower_line = line.lower()
+            blanks = re.sub(r'[a-z]', '_', lower_line)
+            drawing.add(Text(blanks,
+                             (margin, (i*3+2)*line_height),
+                             font_family='Courier',
+                             font_size=line_height,
+                             letter_spacing=line_height/10))
+            nbsp = '\xa0'
+            chars = list(re.sub(r'[^a-z]', nbsp, lower_line))
+            for j, char in enumerate(chars):
+                column_letters[j].append(char)
+            for match in re.finditer(r'[a-z]+', lower_line):
+                start, end = match.span()
+                chars[start:end] = sorted(chars[start:end])
+            sorted_line = ''.join(chars)
+            drawing.add(Text(sorted_line,
+                             (margin, (i*3+3)*line_height),
+                             font_family='Courier',
+                             font_size=line_height,
+                             letter_spacing=line_height/10))
+        for column in column_letters:
+            column.sort()
+        for i in range(row_count):
+            row_letters = ''.join(column[i] for column in column_letters)
+            drawing.add(Text(row_letters,
+                             (margin, (row_count*3+2+i)*line_height),
+                             font_family='Courier',
+                             font_size=line_height,
+                             letter_spacing=line_height/10))
+
+        return drawing.tostring()
--- test/test_blender_block.py
+++ test/test_blender_block.py
@@ -1,7 +1,13 @@
 from io import StringIO
 from textwrap import dedent
 
+from svgwrite import Drawing
+from svgwrite.shapes import Rect
+from svgwrite.text import Text
+
 from blender_block import BlenderBlock
+from live_svg import LiveSvg
+from svg_diagram import SvgDiagram
 
 
 def test_text():
@@ -13,18 +19,18 @@ def test_text():
         Donec ultricies magna vitae risus vestibulum congue. Morbi sed metus
         nulla. Nullam ut felis non quam auctor euismod. Vestibulum ante ipsum
         primis in faucibus orci luctus et ultrices posuere cubilia curae.""")
-    expected_lines1 = ('Lorem ipsum dolor sit amet, consectetur',
-                       'adipiscing elit. Mauris lacus augue,',
-                       'sagittis at tortor id, condimentum',
-                       'mollis nibh. Sed lectus metus, bibendum',
+    expected_lines1 = ('Lorem ipsum dolor sit amet, consectetur ',
+                       'adipiscing elit. Mauris lacus augue,    ',
+                       'sagittis at tortor id, condimentum      ',
+                       'mollis nibh. Sed lectus metus, bibendum ',
                        'sit amet finibus venenatis, vehicula vel',
-                       'ipsum.',
-                       'Donec ultricies magna vitae risus',
-                       'vestibulum congue. Morbi sed metus')
-    expected_lines2 = ('nulla. Nullam ut felis non quam auctor',
+                       'ipsum.                                  ',
+                       'Donec ultricies magna vitae risus       ',
+                       'vestibulum congue. Morbi sed metus      ')
+    expected_lines2 = ('nulla. Nullam ut felis non quam auctor  ',
                        'euismod. Vestibulum ante ipsum primis in',
                        'faucibus orci luctus et ultrices posuere',
-                       'cubilia curae.')
+                       'cubilia curae.                          ')
     blocks = BlenderBlock.read(StringIO(text))
 
     assert len(blocks) == 2
@@ -42,15 +48,15 @@ def test_text_dimensions():
         nulla. Nullam ut felis non quam auctor euismod. Vestibulum ante ipsum
         primis in faucibus orci luctus et ultrices posuere cubilia curae.""")
     expected_lines1 = ('Lorem ipsum dolor sit amet, consectetur adipiscing',
-                       'elit. Mauris lacus augue, sagittis at tortor id,',
-                       'condimentum mollis nibh. Sed lectus metus,',
-                       'bibendum sit amet finibus venenatis, vehicula vel',
-                       'ipsum.',
-                       'Donec ultricies magna vitae risus vestibulum')
+                       'elit. Mauris lacus augue, sagittis at tortor id,  ',
+                       'condimentum mollis nibh. Sed lectus metus,        ',
+                       'bibendum sit amet finibus venenatis, vehicula vel ',
+                       'ipsum.                                            ',
+                       'Donec ultricies magna vitae risus vestibulum      ')
     expected_lines2 = ('congue. Morbi sed metus nulla. Nullam ut felis non',
-                       'quam auctor euismod. Vestibulum ante ipsum primis',
-                       'in faucibus orci luctus et ultrices posuere',
-                       'cubilia curae.')
+                       'quam auctor euismod. Vestibulum ante ipsum primis ',
+                       'in faucibus orci luctus et ultrices posuere       ',
+                       'cubilia curae.                                    ')
     width = 50
     height = 6
     blocks = BlenderBlock.read(StringIO(text), width, height)
@@ -58,3 +64,63 @@ def test_text_dimensions():
     assert len(blocks) == 2
     assert blocks[0].lines == expected_lines1
     assert blocks[1].lines == expected_lines2
+
+
+def test_draw(image_differ):
+    expected_drawing = Drawing(size=(440, 200))
+    grey = 'rgb(200, 200, 200)'
+    expected_drawing.add(Rect((80, 0),
+                              (70, 200),
+                              fill=grey))
+    expected_drawing.add(Rect((220, 0),
+                              (70, 200),
+                              fill=grey))
+    expected_drawing.add(Rect((360, 0),
+                              (70, 200),
+                              fill=grey))
+    expected_drawing.add(Rect((0, 0),
+                              (440, 140),
+                              fill_opacity=0,
+                              stroke='black'))
+    expected_drawing.add(Rect((0, 0),
+                              (440, 200),
+                              fill_opacity=0,
+                              stroke='black'))
+    expected_drawing.add(Text('____ ___ _ "______" ____.',
+                              (10, 40),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_drawing.add(Text('amry adh a\xa0 eilltt\xa0 ablm',
+                              (10, 60),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_drawing.add(Text('___ ______ ___ _____ __ ____.',
+                              (10, 100),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_drawing.add(Text('ist ceeefl asw ehitw as nosw',
+                              (10, 120),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_drawing.add(Text('iaryfhadca waitthetelambsnow',
+                              (10, 160),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_drawing.add(Text('mts\xa0 lee e\xa0 ls wli \xa0 as',
+                              (10, 180),
+                              font_family='Courier',
+                              font_size=20,
+                              letter_spacing=2))
+    expected_svg = expected_drawing.tostring()
+
+    block = BlenderBlock(('Mary had a "little" lamb.     ',
+                          'Its fleece was white as snow. '))
+    actual_svg = block.as_svg()
+
+    image_differ.assert_equal(LiveSvg(SvgDiagram(actual_svg)),
+                              LiveSvg(SvgDiagram(expected_svg)))
